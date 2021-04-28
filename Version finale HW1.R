415687bbb0909480bac4da8e4b9699e2f3beb5cb
mattjs07 <mattis.gjlbert@gmail.com>
2021-04-28 15:01:42 +0200

    adding additional graphs and tables to HW1

--- Version finale HW1.R
+++ Version finale HW1.R
@@ -7,6 +7,7 @@ library(gridExtra)
 library(future) # for parallelization
 library(furrr)
 library(beepr)
+library(gtable)
 
 
 setwd("C:/Users/matti/Desktop/M2_S2/Financial_Econ/Replication_paper")
@@ -115,7 +116,7 @@ PU_loader_end <- function(IDlist,  gamma = 0.61, delta = 0.69, alpha = 0.5, df, 
   return(rbindlist(PU_loader(liste = IDlist)))        # This binds all the computed dataframes together, to make the final dataframe.
 }
 
-ID <- table(df$PERMNO) %>%  as.matrix() %>% rownames() %>% as.integer %>%  as.list()  # All the PERMNOs
+ID <- as.list(unique(data$PERMNO))  # All the PERMNOs
 
 ### PU for Q1 and 2 ###
 
@@ -144,7 +145,7 @@ data <- fread("Prospect_Allv2.csv")   ## The dataset with the prospective utilit
 data <- mutate(data, cap = SHROUT * PRC, ri_rf = RET - rf) #Adding a column for excess returns called ri_rf
 
 
-DATES <- table(data$date)  %>%  as.matrix() %>% rownames() %>% as.integer %>%  as.list() #list with all DATES, needed for following functions
+DATES <- as.list(unique(data$date)) #list with all DATES, needed for following functions
 
 Quantiles_All =  function(liste, df){     #This function assign PERMNO to a decile for Every t 
   
@@ -162,7 +163,7 @@ Quantiles_All =  function(liste, df){     #This function assign PERMNO to a deci
 data <- Quantiles_All(liste = DATES, df = data) 
 data <- arrange(data, PERMNO, date)
 
-data <- mutate(group_by(data,PERMNO), RET_T1 = lead(RET,1),  rf_T1 = lead(rf,1)) %>%  ungroup() %>% relocate(RET_T1, .after = RET) %>%  relocate(rf_T1, .after = rf)
+data <- mutate(group_by(data,PERMNO), RET_T1 = lead(RET,1),  rf_T1 = lead(rf,1), ri_rf_T1 = lead(ri_rf,1)) %>%  ungroup() %>% relocate(RET_T1, .after = RET) %>%  relocate(rf_T1, .after = rf)
 #New columns with returns at t+1 and risk free at t+1.
 
 stat1 <- data %>% group_by(deciles) %>% summarise( E_ret1 = mean(RET_T1, na.rm = TRUE    ), E_ri_rf = mean(ri_rf),  E_rm = mean(rm_rf))
@@ -186,6 +187,59 @@ p_a <- ggplot(data = cbind(dd, x = 1:10), aes(y = alpha, x= x)) + geom_point() +
 
 grid.arrange(tableGrob(dd), p_b, p_a, nrow = 2, layout_matrix = rbind(c(1,1), c(2,3)))
 
+### Prospective utility evolution by deciles 
+
+graph <- data[, .(mean_U = mean(U), mean_RET_t1 = mean(RET_T1, na.rm = TRUE)), by = .(deciles, date)]
+graph$deciles <- as.factor(graph$deciles)
+
+cc <- scales::seq_gradient_pal("blue", "yellow", "Lab")(seq(0,1,length.out = 10))
+cc[1] <- "cyan1"
+cc[10] <- "red"
+
+Av_U <- ggplot(graph, aes(x = date, y = mean_U, color = deciles)) + geom_smooth(size = 1, se = F) + geom_line() + 
+  scale_colour_manual(values=cc) + labs(x = "date", y = "Average Prospective Utility", title = "Evolution of the Average Prospective Utility by deciles") + 
+  theme(plot.title = element_text(hjust = 0.5))  + guides(colour = guide_legend(reverse=T))
+
+Av_RET_t1 <- ggplot(graph, aes(x = date, y = mean_RET_t1, color = deciles)) + geom_smooth(size = 0.75, se = F) + 
+  scale_colour_manual(values=cc) + labs(x = "date", y = "Average Returns at t+1", title = "Average Returns at t+1 by deciles") + 
+  theme(plot.title = element_text(hjust = 0.5))  + guides(colour = guide_legend(reverse=T))
+
+grid.arrange(Av_U, Av_RET_t1, nrow = 2)
+
+
+ret_by_date <- data %>% mutate(year = substr(date, 0, 4) )
+  
+  
+ret_by_date <- ret_by_date[, .(av_ret = mean(RET), var_ret = var(RET), n = .N), by = year]
+
+
+ret_plot <- ggplot(ret_by_date) + geom_col(aes(x = year, y = av_ret)) +
+  theme(axis.text.x = element_text(angle=90)) + labs(y = "Average returns", title = "Average returns per year") +
+  theme(plot.title = element_text(hjust = 0.5)) +   theme(axis.text.x = element_blank(), 
+                                                          axis.ticks.x = element_blank(), 
+                                                          axis.title.x = element_blank())
+
+add_plot <-  ggplot(ret_by_date) + geom_col(aes(x = year,y = var_ret), fill = "lightblue") + 
+  geom_col(aes(x = year,y = n/nrow(data)), fill = "red", alpha = 0.2) +
+  theme(axis.text.x = element_text(angle=90)) + labs(y = "Variance and share of observations") +
+  theme(legend.background = element_rect("grey"))  + 
+  labs(caption = "In the bottom graph the variance of returns in each year is displayed in blue. The share of observations for a given year is displayed in red (as might explain size of variance)") + 
+  theme(plot.caption = element_text(hjust = 0))
+
+
+ggplot(ret_by_date) + geom_smooth( aes(x = n, y = var_ret))
+lm( data  = ret_by_date, var_ret ~ n ) %>%  summary()
+
+
+g2 <- ggplotGrob(ret_plot)
+g3 <- ggplotGrob(add_plot)
+g <- rbind(g2, g3, size = "first")
+grid.arrange(g)
+
+descriptive <- data[, .(mean_U = mean(U), mean_RET = mean(RET, na.rm = T), mean_RET_t1 = mean(RET_T1, na.rm = TRUE),
+                        mean_ri_rf = mean(ri_rf, na.rm = T),  mean_ri_rf_t1 = mean(ri_rf_T1, na.rm = T), mean_PRC = mean(PRC, na.rm = T)), by = .(deciles)]
+
+tableGrob(descriptive) %>% grid.arrange()
 
 #####################################
 ########### QUESTION 2 ##############
@@ -196,19 +250,19 @@ strategy <- function(df){   # A function that returns a dataframe with sharpe ra
                                                     #Allows to code only once and reuse the function in Q2 / Q3 / Q4 
   df <- mutate(df, ri_rf = RET - rf)
   
-  DATES <- table(df$date)  %>%  as.matrix() %>% rownames() %>% as.integer %>%  as.list()
+  DATES <- as.list(unique(data$date))
   
   df <- Quantiles_All(liste = DATES, df = df)
   df <- arrange(df, PERMNO, date)
   
-  df <- mutate(group_by(df,PERMNO), RET_T1 = lead(RET,1),  rf_T1 = lead(rf,1)) %>%  ungroup() %>% relocate(RET_T1, .after = RET) %>%  relocate(rf_T1, .after = rf)
+  df <- mutate(group_by(df,PERMNO), RET_T1 = lead(RET,1),  rf_T1 = lead(rf,1), rm_rf_T1 = lead(rm_rf, 1)) %>%  ungroup() %>% relocate(RET_T1, .after = RET) %>%  relocate(rf_T1, .after = rf) %>% relocate(rm_rf_T1, .after = rm_rf) 
   
   D1_10 <- filter(df, deciles == 1 | deciles == 10)       #Keep observations in 1st or 10th decile
   D1_10 <- mutate(df, Long_Short = ifelse(deciles == 1, RET_T1 - rf_T1, rf_T1 - RET_T1 ))  #The returns from Long/Short strategy
   
   sharpe_ratio <- data.frame( sharpe_ratio = mean(D1_10$Long_Short, na.rm = TRUE)/ sqrt(var(D1_10$Long_Short, na.rm = TRUE)))
   excess_ret <- data.frame( excess_ret = mean(D1_10$Long_Short, na.rm = TRUE)) #Excess returns computed from Long-short strategy's returns
-  alpha = lm(data = D1_10 , Long_Short ~ rm_rf) %>% summary()
+  alpha = lm(data = D1_10 , Long_Short ~ rm_rf_T1) %>% summary()
   alpha <- data.frame( alpha = alpha$coefficients[1,1])
   
   return(cbind(sharpe_ratio, excess_ret, alpha))
